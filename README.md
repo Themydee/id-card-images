6ffb5f67e49cebf726e403814e8e53464549f323
Joshua Aideloje <91752742+JoshIri360@users.noreply.github.com>
2023-03-14 22:35:04 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# Image Gallery
+
+This is a simple web app that allows you to relive your hilarious and embarrassing moments from your first year of school! With our new app, you can browse and share funny ID card photos, and compare them to your current self. See how much you've grown and changed over the years!
+
+## Usage
+
+1. Clone this repository to your local machine.
+2. Open the `index.html` file in your web browser.
+3. Use the app to browse and share funny ID card photos.
+
+## Features
+
+- Browse and share funny ID card photos
+- Compare your past self to your current self
+- See how much you've grown and changed over the years
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Credits
+
+This app was created by [Your Name Here].
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ script.js
@@ -0,0 +1,39 @@
+// Constants for the range of image IDs
+const START_ID = 1900001;
+const END_ID = 1901800;
+
+// The image gallery element
+const imageGallery = document.getElementById("image-gallery");
+
+// Loop through each image ID in the range and fetch the corresponding image
+for (let i = START_ID; i <= END_ID; i++) {
+  // Create an image element for the fetched image
+  if (i == 1900261 || i == 1901771) {
+    continue;
+  }
+  const image = document.createElement("img");
+
+  // Create a promise for fetching the image
+  const promise = new Promise((resolve, reject) => {
+    image.onload = resolve;
+    image.onerror = reject;
+    image.src = `https://fp.lmu.edu.ng/clearapi/images/app/${i}.JPG`;
+  });
+
+  // Append the image element to the gallery as soon as it is loaded
+  promise.then(() => {
+    // Create a span element to display the image ID
+    const imageID = document.createElement("span");
+    imageID.innerHTML = `Reg No.: ${i}`;
+
+    // Create a container div for the image and ID
+    const container = document.createElement("div");
+    container.classList.add("image");
+    container.appendChild(image);
+    container.appendChild(imageID);
+
+    // Add the container to the image gallery
+    imageGallery.appendChild(container);
+    container.classList.add("fade-in");
+  });
+}
